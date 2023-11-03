7baec6938c5c4e55ca6fc8c9693582ce9b59873e
reza.muhamad <reza.muhamad@pactindo.com>
2023-11-03 16:48:26 +0700

    validasi login

--- src/application/components/LoginBox.tsx
+++ src/application/components/LoginBox.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../assets/css/HomeCss.css'
-import {TextField} from '@mui/material';
+import {TextField, FormHelperText} from '@mui/material';
 import {useNavigate } from 'react-router-dom'
 
 const LoginBox: React.FC = () => {
-    // const []
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [message, setMessage] = useState("")
     
     const navigate = useNavigate ()
 
     const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault() 
+        e.preventDefault()
+        if(username == "" || password == ""){
+            return setMessage("Silahkan lengkapi username dan password!")
+        }
         navigate('/dashboard')
     }
     return (
@@ -18,7 +23,7 @@ const LoginBox: React.FC = () => {
                 <h2>Login</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="user-box">
-                        <TextField id="outlined-basic" label="Username" variant="outlined" size='small' fullWidth  required/>
+                        <TextField id="outlined-basic" label="Username" onInput={(e: any) => setUsername(e.target?.value)} value={username} variant="outlined" size='small' fullWidth />
                     </div>
                     <div className="user-box">
                     <TextField
@@ -28,10 +33,12 @@ const LoginBox: React.FC = () => {
                         autoComplete="current-password"
                         fullWidth
                         size='small'
-                        required
+                        onInput={(e: any) => setPassword(e.target?.value)}
+                        value={password}
                         />
                     </div>
                     <button type="submit" >Login</button>
+                    <FormHelperText sx={{color : '#b30000'}}>{message}</FormHelperText>
                 </form>
             </div>
         </>
