2740b6270270b0baf0f88bc79e7db21832c36079
reza.muhamad <reza.muhamad@pactindo.com>
2024-01-02 15:16:03 +0700

    fixing error ts carousel

--- src/application/containers/DetailProject.tsx
+++ src/application/containers/DetailProject.tsx
@@ -9,16 +9,9 @@ import {
   Grid,
   Button
 } from '@mui/material'
-import Slider from "react-slick";
+import Carousel from '../components/Carousel';
 
 const DetailProject: React.FC = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
   return (
     <>
       <motion.div
@@ -28,32 +21,13 @@ const DetailProject: React.FC = () => {
         transition={{ type: "spring", stiffness: 100, duration: 0.2 }}
       >
         <Navigation />
-        {/* <Container fixed>
+        <Container fixed>
           <h1>Detail Projects</h1>
           <Breadcrumbs title='Detail Projects' icon={<ApartmentIcon sx={{ mr: 0.5 }} fontSize="inherit" />} />
-          <Card sx={{ height: 'auto' }}> */}
-            <Slider {...settings}>
-              <div>
-                <h3>1</h3>
-              </div>
-              <div>
-                <h3>2</h3>
-              </div>
-              <div>
-                <h3>3</h3>
-              </div>
-              <div>
-                <h3>4</h3>
-              </div>
-              <div>
-                <h3>5</h3>
-              </div>
-              <div>
-                <h3>6</h3>
-              </div>
-            </Slider>
-          {/* </Card>
-        </Container> */}
+          <Card sx={{ height: 'auto' }}>
+            <Carousel />
+          </Card>
+        </Container>
       </motion.div>
     </>
   )
